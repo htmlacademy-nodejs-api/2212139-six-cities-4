540cd18eec1f76d72a56d6938815210b894ad205
Alexey_Makeev <makeevhome@mail.ru>
2023-05-04 19:59:08 +0300

    add color (chalk)

--- src/core/cli-command/helper.command.ts
+++ src/core/cli-command/helper.command.ts
@@ -1,10 +1,10 @@
 import { CliCommandInterface } from './cli-command.interface.js';
-
+import chalk from 'chalk';
 export default class HelpCommand implements CliCommandInterface {
   public readonly name = '--help';
 
   public async execute(): Promise<void> {
-    console.log(`
+    console.log(chalk.green(`
         Программа для подготовки данных для REST API сервера.
         Пример:
             main.js --<command> [--arguments]
@@ -13,6 +13,6 @@ export default class HelpCommand implements CliCommandInterface {
             --help:                      # печатает этот текст
             --import <path>:             # импортирует данные из TSV
             --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных
-        `);
+        `));
   }
 }
--- src/core/cli-command/version.command.ts
+++ src/core/cli-command/version.command.ts
@@ -1,6 +1,7 @@
 import { readFileSync } from 'node:fs';
 import { CliCommandInterface } from './cli-command.interface.js';
 import path from 'node:path';
+import chalk from 'chalk';
 
 export default class VersionComand implements CliCommandInterface {
   public readonly name = '--version';
@@ -13,6 +14,6 @@ export default class VersionComand implements CliCommandInterface {
 
   public async execute(): Promise<void> {
     const version = this.readVersion();
-    console.log(version);
+    console.log(chalk.green(version));
   }
 }
