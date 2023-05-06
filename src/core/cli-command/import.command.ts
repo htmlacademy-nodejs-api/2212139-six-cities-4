8603912e1a3a20476a258d5239443efbcba644c6
Alexey_Makeev <makeevhome@mail.ru>
2023-05-06 22:08:13 +0300

    add file-writer.ts

--- src/core/cli-command/import.command.ts
+++ src/core/cli-command/import.command.ts
@@ -1,22 +1,30 @@
 import TSVFileReader from '../file-reader/tsv-file-reader.js';
 import { CliCommandInterface } from './cli-command.interface.js';
 import chalk from 'chalk';
+import { createOffer } from '../helpers/offers.js';
+import { getErrorMessage } from '../helpers/common.js';
+
 export default class ImportCommand implements CliCommandInterface {
   public readonly name = '--import';
-  public execute(filename: string): void {
 
-    const fileReader = new TSVFileReader(filename.trim());
+  private onLine(line: string) {
+    const offer = createOffer(line);
+    console.log(offer);
+  }
+
+  private onComplete(count: number) {
+    console.log(`${count} row imported.`);
+  }
+
+  public async execute(fileName: string): Promise<void> {
+    const fileReader = new TSVFileReader(fileName.trim());
+    fileReader.on('line', this.onLine);
+    fileReader.on('end', this.onComplete);
 
     try {
-      fileReader.read();
-      console.log(fileReader.toArray());
+      await fileReader.read();
     } catch (err) {
-
-      if (!(err instanceof Error)) {
-        throw err;
-      }
-
-      console.log(chalk.red(`Не удалось импортировать данные из файла по причине: «${err.message}»`));
+      console.log(chalk.red(`can't read the file ${getErrorMessage(err)}`));
     }
   }
 }
