d66a3e28cae111976d93357d0cc218598ce3b3be
Alexey_Makeev <makeevhome@mail.ru>
2023-05-23 18:08:55 +0300

    add comment modules

---
+++ src/modules/comment/comment.service.ts
@@ -0,0 +1,31 @@
+import { inject, injectable } from 'inversify';
+import { CommentServiceInterface } from './comment-service.interface.js';
+import { AppComponent } from '../../types/app-component.enum.js';
+import { CommentEntity } from './comment.entity.js';
+import { DocumentType, types } from '@typegoose/typegoose';
+import CreateCommentDto from './dto/create-comment.dto.js';
+
+@injectable()
+export default class CommentService implements CommentServiceInterface {
+  constructor(
+    @inject(AppComponent.CommentModel)
+    private readonly commentModel: types.ModelType<CommentEntity>
+  ) {}
+
+  public async create(
+    dto: CreateCommentDto
+  ): Promise<DocumentType<CommentEntity>> {
+    const comment = await this.commentModel.create(dto);
+    return comment.populate('userId');
+  }
+
+  public async findByOfferId(offerId: string): Promise<DocumentType<CommentEntity>[]> {
+    return this.commentModel.find({offerId}).populate('userId');
+  }
+
+  public async deleteByOfferId(offerId: string): Promise<number> {
+    const result = await this.commentModel.deleteMany({offerId}).exec();
+
+    return result.deletedCount;
+  }
+}
